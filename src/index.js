aa2e5bbce4cbce8e55d478568df1a89cf721e3fb
Feng Xiong <fengxiong34@gmail.com>
2021-01-28 09:53:48 -0800

    fixed routers

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import AdminLayout from "layouts/Admin/Admin.js";
 import Login from "./views/Login";
+import UserProfile from "./views/UserProfile";
 import ResetPassword from "./components/ResetPassword";
+import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
+import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+import { Provider } from "react-redux";
+import { configureStore } from "./reducers/index";
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "antd/dist/antd.css";
-import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
-import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
-import { Provider } from "react-redux";
-import { configureStore } from "./reducers/index";
 
 export const store = configureStore();
 
@@ -24,6 +25,7 @@ ReactDOM.render(
           <Switch>
             <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
             <Route path="/login" render={(props) => <Login {...props}></Login>} />
+            <Route path="/user-profile" render={() => <UserProfile />} />
             <Route path="/resetPassword" render={() => <ResetPassword></ResetPassword>} />
             <Redirect from="/" to="/login" />
           </Switch>
